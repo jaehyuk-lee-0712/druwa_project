0b33cb8f50e2ce9b8f95db707f0f120cddabe4d6
jaehyuk-lee-0712 <57936553+jaehyuk-lee-0712@users.noreply.github.com>
2024-07-09 02:50:00 +0900

    commit
    commit

--- bes/index.js
+++ bes/index.js
@@ -34,6 +34,28 @@ const callDruwaDataFile = async () => {
     const dataList = response.data;
 
     if (dataList.length > 0) {
+        // for(let i =0; i < dataList.length; i++) {
+        for(let i =0; i < 10; i++) {
+
+          const rotateInfo = await getAddressCoordinates(dataList[i].storeAddress);
+
+          const dtName = dataList[i].storeName;
+          const dtAddress = dataList[i].storeAddress;
+          const dtLat = rotateInfo.latitude;
+          const dtLon = rotateInfo.longitude;
+
+          const newStroe = new DTstroeBasic({
+            dtName , 
+            dtAddress , 
+            dtLat ,
+            dtLon
+          })
+          
+          const saveStore = await newStroe.save();
+
+          console.log(saveStore);
+
+        }
     }
   } catch (error) {
     console.error("Error Call Data : ", error);
@@ -41,8 +63,43 @@ const callDruwaDataFile = async () => {
   }
 };
 
+callDruwaDataFile();
+
 // 위도 , 경도 API
-const getRotateInfo = () => {};
+// API 키 설정
+const CLIENT_ID = 'n3baurqzz3';
+const CLIENT_SECRET = 'huvYHl0HzBfJARxNAmYsq7ZbvEa7Bmcq1AQ4kFEw';
+
+// 헤더 설정
+const headers = {
+  "X-NCP-APIGW-API-KEY-ID": CLIENT_ID,
+  "X-NCP-APIGW-API-KEY": CLIENT_SECRET,
+};
+
+// API URL 설정
+const apiUrl = 'https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode';
+
+async function getAddressCoordinates(address) {
+  try {
+    // 요청 파라미터 설정
+    const params = {
+      query: address
+    };
+    
+    // API 요청
+    const response = await axios.get(apiUrl, { params, headers });
+    
+    // 응답 처리
+    if (response.data.status === "OK" && response.data.meta.totalCount > 0) {
+      const { x, y } = response.data.addresses[0];
+      return { latitude: y, longitude: x }; 
+    } else {
+      console.log("주소에 해당하는 결과를 찾을 수 없습니다.");
+    }
+  } catch (error) {
+    console.error(`API 요청 중 오류 발생: ${error}`);
+  }
+}
 
 // port setting.
 app.listen(9000, () => {
